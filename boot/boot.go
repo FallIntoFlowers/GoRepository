c1af8ee7ac3f63ba6aeb2514bd661f71dc26ffa1
FallIntoFlowers <39542446+FallIntoFlowers@users.noreply.github.com>
2018-09-09 20:07:03 +0800

    Update boot.go

--- boot/boot.go
+++ boot/boot.go
@@ -21,6 +21,7 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 }
 
 func boot() {
+	fmt.Printf();
 	http.HandleFunc("/", sayhelloName) //设置访问的路由
 	err := http.ListenAndServe(":9090", nil) //设置监听的端口
 	if err != nil {
